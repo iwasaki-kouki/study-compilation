4bf38dedcac4c5446738581dbd630bf8a52f3de5
岩崎 <karausuyanosegare@gmail.com>
2022-08-29 14:54:28 +0900

    #5

--- src/main/java/com/example/whenandwhattime/entity/Livers.java
+++ src/main/java/com/example/whenandwhattime/entity/Livers.java
@@ -1,41 +1,43 @@
 package com.example.whenandwhattime.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
-@Table(name = "Livers")
+@Table(name = "livers")
 @Data
-public class Livers{
-	
-    public Livers(String name, String ch_name, String twitter_url,String youtube_url) {
-	        this.liversname = name;
-	        this.ch_name = ch_name;
-	        this.twitter = twitter_url;
-	        this.youtube = youtube_url;
-    }
+public class Livers implements Serializable{
+	private static final long serialVersionUID = 1L;
+
     
     @Id
     @SequenceGenerator(name = "Livers_id_seq")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long liverId;
-    
-    @Column(nullable = false)
-    private String liversname;
-    
-    @Column(nullable = false)
-    private String ch_name;
+    private Long Id;
 
     @Column(nullable = false)
     private String twitter;
     
     @Column(nullable = false)
     private String youtube;
+    
+    @Column(nullable = false)
+    private String thumbnail;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", insertable = false, updatable = false)
+    private User user;
+    
+    
 }
---
+++ src/main/java/com/example/whenandwhattime/form/LiverForm.java
@@ -0,0 +1,35 @@
+package com.example.whenandwhattime.form;
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.whenandwhattime.validation.constraints.ImageByte;
+import com.example.whenandwhattime.validation.constraints.ImageNotEmpty;
+
+import lombok.Data;
+
+@Data
+public class LiverForm {
+
+    private Long id;
+    
+    private Long userId;
+    
+    @ImageNotEmpty
+    @ImageByte(max = 2000000)
+    private MultipartFile image;
+
+    private String imageData;
+
+    private String path;
+    
+    private String name;
+    
+    private String twitter_URL;
+    
+    private String youtube_URL;
+
+    private UserForm user;
+
+
+}
